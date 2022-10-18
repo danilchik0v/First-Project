4f3720a81ce1614b27d556396992a53efeef2c67
danilchik0v <116120126+danilchik0v@users.noreply.github.com>
2022-10-19 00:18:12 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var X=2002, Y=3, Z=15
+document.write(X+Y+Z)
+document.write('<br>')
+document.write(X-Y+Z)
+document.write('<br>')
+document.write(X/Y-Z)
+document.write('<br>')
+document.write(X/Y*Z)
+document.write('<br>')
+document.write(X%Y+Z)
+
